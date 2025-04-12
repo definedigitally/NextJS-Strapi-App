05e1c5850a6930fcd1f53c4cfe2c3bba89eca374
coderomm <mail.coderom@gmail.com>
2025-04-12 19:00:34 +0530

    fix: update for prod cors

--- infinity-app/next.config.ts
+++ infinity-app/next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from "next";
 
+const STRAPI_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
-        pathname: "/uploads/**",
+        protocol: STRAPI_URL.includes('localhost') ? 'http' : 'https',
+        hostname: STRAPI_URL,
+        pathname: '/uploads/**',
       },
     ],
   },
